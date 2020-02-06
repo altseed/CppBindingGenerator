e5109131d87e34ba905b45ed8b5d4bfe1a9c28ad
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-02-07 00:02:02 +0900

    fix:attribute

--- cbg/binding_generator_csharp.py
+++ cbg/binding_generator_csharp.py
@@ -64,8 +64,7 @@ class BindingGeneratorCSharp(BindingGenerator):
             code('/// <summary>')
             code('/// {}'.format(enum_.brief.descs[self.lang]))
             code('/// </summary>')
-        
-        code('System.Serializable')
+        code('[System.Serializable]')
         with CodeBlock(code, 'public enum {} : int'.format(enum_.name)):
             for val in enum_.values:
                 # XML Comment
