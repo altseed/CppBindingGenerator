6146274b657bc472ca4146aaa832740090b5ab7b
durswd <swda.durl@gmail.com>
2020-04-27 02:35:19 +0900

    remove com

--- cbg/cpp_binding_generator.py
+++ cbg/cpp_binding_generator.py
@@ -627,32 +627,6 @@ extern "C" {
 
 }
 
-#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
-
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-	bool res = true;
-	switch (fdwReason) {
-	case DLL_PROCESS_ATTACH:
-		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |COINIT_DISABLE_OLE1DDE);
-		break;
-	case DLL_PROCESS_DETACH:
-		CoUninitialize();
-		break;
-	case DLL_THREAD_ATTACH:
-		CoInitializeEx(NULL, COINIT_APARTMENTTHREADED |COINIT_DISABLE_OLE1DDE);
-		break;
-	case DLL_THREAD_DETACH:
-		CoUninitialize();
-		break;
-	default:
-		break;
-	}
-	return res;
-}
-
-#endif
-
 '''
 
         code += footer
